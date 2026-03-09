bec312a73084e7160904009e3766217b79a59534
mistourr-app <mistourr@gmail.com>
2026-03-09 21:51:52 +0300

    Update level balance: Sector 1 easier, add empty cells display in admin

--- registry.js
+++ registry.js
@@ -1,46 +1,221 @@
 // Временное хранилище данных об уровнях.
 // В будущем это будет загружаться из localStorage или сервера.
 export const LEVELS = [
-  { 
-    id: 'tutorial', 
-    rows: 13, 
-    name: "Обучение", 
-    bossHpMultiplier: 1.0,
-    isTutorial: true,
-    layout: [
-      // Ряд 0: старт
-      [{}, {}, {}, {}, {}],
-      // Ряд 1: пустой
-      [{}, {}, {}, {}, {}],
-      // Ряд 2: пустой (для прыжка)
-      [{}, {}, {}, {}, {}],
-      // Ряд 3: здоровье
-      [{}, {}, {type: 'heal'}, {}, {}],
-      // Ряд 4: патроны
-      [{}, {}, {type: 'ammo'}, {}, {}],
-      // Ряд 5: враг по центру
-      [{}, {}, {type: 'enemy', enemyType: 'TYPE_2'}, {}, {}],
-      // Ряд 6: пустой
-      [{}, {}, {}, {}, {}],
-      // Ряд 7: враг слева (для выстрела из арбалета)
-      [{type: 'enemy', enemyType: 'TYPE_1'}, {}, {}, {}, {}],
-      // Ряд 8: пустой
-      [{}, {}, {}, {}, {}],
-      // Ряд 9: бонус атаки
-      [{}, {}, {type: 'attack_bonus'}, {}, {}],
-      // Ряд 10: враг
-      [{}, {}, {type: 'enemy', enemyType: 'TYPE_2'}, {}, {}],
-      // Ряд 11-12: арена босса
-      // Ряд 12 (индекс 11) - ряд игрока с бонусами и здоровьем
-      [{type: 'attack_bonus', data: {value: 5}}, {type: 'attack_cell', data: {value: 10}}, {type: 'heal', data: {amount: 17}}, {type: 'attack_cell', data: {value: 10}}, {type: 'attack_bonus', data: {value: 5}}],
-      // Ряд 13 (индекс 12) - ряд босса, клетки атаки только на краях (x=0, x=4)
-      [{type: 'attack_cell', data: {value: 5}}, {type: 'heal'}, {}, {type: 'heal'}, {type: 'attack_cell', data: {value: 5}}]
+  {
+    "id": "tutorial",
+    "rows": 13,
+    "name": "Обучение",
+    "bossHpMultiplier": 1,
+    "isTutorial": true,
+    "layout": [
+      [
+        {},
+        {},
+        {},
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {},
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {},
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {
+          "type": "heal"
+        },
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {
+          "type": "ammo"
+        },
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {
+          "type": "enemy",
+          "enemyType": "TYPE_2"
+        },
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {},
+        {},
+        {}
+      ],
+      [
+        {
+          "type": "enemy",
+          "enemyType": "TYPE_1"
+        },
+        {},
+        {},
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {},
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {
+          "type": "attack_bonus"
+        },
+        {},
+        {}
+      ],
+      [
+        {},
+        {},
+        {
+          "type": "enemy",
+          "enemyType": "TYPE_2"
+        },
+        {},
+        {}
+      ],
+      [
+        {
+          "type": "attack_bonus",
+          "data": {
+            "value": 5
+          }
+        },
+        {
+          "type": "attack_cell",
+          "data": {
+            "value": 10
+          }
+        },
+        {
+          "type": "heal",
+          "data": {
+            "amount": 17
+          }
+        },
+        {
+          "type": "attack_cell",
+          "data": {
+            "value": 10
+          }
+        },
+        {
+          "type": "attack_bonus",
+          "data": {
+            "value": 5
+          }
+        }
+      ],
+      [
+        {
+          "type": "attack_cell",
+          "data": {
+            "value": 5
+          }
+        },
+        {
+          "type": "heal"
+        },
+        {},
+        {
+          "type": "heal"
+        },
+        {
+          "type": "attack_cell",
+          "data": {
+            "value": 5
+          }
+        }
+      ]
     ]
   },
-  { id: 'test_arena', rows: 6, name: "Тестовый полигон", bossHpMultiplier: 1.5, hidden: true, chances: { ENEMY: 0.1, WALL: 0.1, HEAL: 0.05, AMMO: 0.04, ENERGY: 0.04, ATTACK_BONUS: 0.03, DEFENSE_BONUS: 0.03 } },
-  { id: 'sector_1', rows: 30, name: "Сектор 1", bossHpMultiplier: 2.5, chances: { ENEMY: 0.1, WALL: 0.1, HEAL: 0.05, AMMO: 0.04, ENERGY: 0.04, ATTACK_BONUS: 0.03, DEFENSE_BONUS: 0.03 } },
-  { id: 'sector_2', rows: 50, name: "Сектор 2", bossHpMultiplier: 3.5, chances: { ENEMY: 0.15, WALL: 0.1, HEAL: 0.05, AMMO: 0.04, ENERGY: 0.04, ATTACK_BONUS: 0.03, DEFENSE_BONUS: 0.03 } },
-  { id: 'core', rows: 75, name: "Ядро", bossHpMultiplier: 5.0, chances: { ENEMY: 0.2, WALL: 0.15, HEAL: 0.05, AMMO: 0.04, ENERGY: 0.04, ATTACK_BONUS: 0.03, DEFENSE_BONUS: 0.03 } }
+  {
+    "id": "test_arena",
+    "rows": 6,
+    "name": "Тестовый полигон",
+    "bossHpMultiplier": 1.5,
+    "hidden": true,
+    "chances": {
+      "ENEMY": 0.1,
+      "WALL": 0.1,
+      "HEAL": 0.05,
+      "AMMO": 0.04,
+      "ENERGY": 0.04,
+      "ATTACK_BONUS": 0.03,
+      "DEFENSE_BONUS": 0.03
+    }
+  },
+  {
+    "id": "sector_1",
+    "rows": 30,
+    "name": "Сектор 1",
+    "bossHpMultiplier": 1.2,
+    "chances": {
+      "ENEMY": 0.1,
+      "WALL": 0.15,
+      "HEAL": 0.06,
+      "AMMO": 0.05,
+      "ENERGY": 0.06,
+      "ATTACK_BONUS": 0.05,
+      "DEFENSE_BONUS": 0.06
+    }
+  },
+  {
+    "id": "sector_2",
+    "rows": 50,
+    "name": "Сектор 2",
+    "bossHpMultiplier": 3.5,
+    "chances": {
+      "ENEMY": 0.15,
+      "WALL": 0.1,
+      "HEAL": 0.05,
+      "AMMO": 0.04,
+      "ENERGY": 0.04,
+      "ATTACK_BONUS": 0.03,
+      "DEFENSE_BONUS": 0.03
+    }
+  },
+  {
+    "id": "core",
+    "rows": 75,
+    "name": "Ядро",
+    "bossHpMultiplier": 5,
+    "chances": {
+      "ENEMY": 0.2,
+      "WALL": 0.15,
+      "HEAL": 0.05,
+      "AMMO": 0.04,
+      "ENERGY": 0.04,
+      "ATTACK_BONUS": 0.03,
+      "DEFENSE_BONUS": 0.03
+    }
+  }
 ];
 
 export function getLevelById(id) {
